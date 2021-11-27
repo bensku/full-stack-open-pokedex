5d38321ef28f7488d31c81c60be727b07794984c
bensku <bmi@iki.fi>
2021-11-27 15:58:34 +0200

    Publish on Heroku

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('green')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
